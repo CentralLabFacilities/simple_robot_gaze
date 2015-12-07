1c9a0e895025f984bad3a66c8c960e4a52da69e4
fl <flier@techfak.uni-bielefeld.de>
2015-12-07 16:29:28 +0100

    Threading

--- srg/control/gaze.py
+++ srg/control/gaze.py
@@ -57,4 +57,4 @@ class GazeController():
                 self.rc.robot_controller.set_gaze_target(current_target, False)
             # Running at 100 Hz Maximum!
             time.sleep(0.01)
-        print(">>> Deactivating Gaze Controller")
+        print">>> Deactivating Gaze Controller to: %s" % self.rc.outscope
