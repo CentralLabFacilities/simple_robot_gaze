b5c6f399de3fedf7881c3caa6d58c9de97bf9221
fl <flier@techfak.uni-bielefeld.de>
2016-01-15 16:52:09 +0100

    Cleanup after RSB horror

--- srg/control/gaze.py
+++ srg/control/gaze.py
@@ -63,7 +63,6 @@ class GazeController(threading.Thread):
                 current_target = self.mw.current_robot_gaze
                 if self.acquire_prio:
                     try:
-                        print current_target
                         self.rc.robot_controller.set_gaze_target(current_target, False)
                     except Exception, e:
                         print ">>> ERROR (set_gaze): %s" % str(e)
