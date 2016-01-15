87db350955326d8c1dce3a3cb169426fbc9be69a
fl <flier@techfak.uni-bielefeld.de>
2016-01-15 15:50:17 +0100

    Working on RSB support

--- srg/control/gaze.py
+++ srg/control/gaze.py
@@ -63,6 +63,7 @@ class GazeController(threading.Thread):
                 current_target = self.mw.current_robot_gaze
                 if self.acquire_prio:
                     try:
+                        print ">>> (setting_gaze)"
                         self.rc.robot_controller.set_gaze_target(current_target, True)
                     except Exception, e:
                         print ">>> ERROR (set_gaze): %s" % str(e)
